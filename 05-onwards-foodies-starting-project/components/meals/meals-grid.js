515bd595b2644e83d337e4e5bef8b14c3c315874
rizkirisjad <rizkirisjad@gmail.com>
2024-05-25 20:49:22 +0700

    setting up sqlite database

---
+++ 05-onwards-foodies-starting-project/components/meals/meals-grid.js
@@ -0,0 +1,19 @@
+/** @format */
+
+import MealItem from './meal-item';
+import classes from './meals-grid.module.css';
+
+const MealsGrid = ({ meals }) => {
+  return (
+    <ul className={classes.meals}>
+      {meals.map((meal) => {
+        return (
+          <li key={meal.id}>
+            <MealItem {...meal} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+export default MealsGrid;
