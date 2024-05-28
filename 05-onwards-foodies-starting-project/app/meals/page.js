49227198df4376bf46b81cf3cc75ef64e10c9afb
rizkirisjad <rizkirisjad@gmail.com>
2024-05-28 10:19:05 +0700

    fetching data

--- 05-onwards-foodies-starting-project/app/meals/page.js
+++ 05-onwards-foodies-starting-project/app/meals/page.js
@@ -2,8 +2,11 @@
 import Link from 'next/link';
 import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
 
-const MealsPage = () => {
+const MealsPage = async () => {
+  const meals = await getMeals();
+
   return (
     <>
       <header className={classes.header}>
@@ -19,7 +22,7 @@ const MealsPage = () => {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
