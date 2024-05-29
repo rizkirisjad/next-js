64d1d50018a7c20d5834de08521cd003c684b361
rizkirisjad <rizkirisjad@gmail.com>
2024-05-29 21:24:14 +0700

    error & not found

---
+++ 05-onwards-foodies-starting-project/app/meals/error.js
@@ -0,0 +1,12 @@
+/** @format */
+'use client';
+
+const Error = () => {
+  return (
+    <main className="error">
+      <h1>An error occurred!</h1>
+      <p>Failed to fetch meal data. Please try again later.</p>
+    </main>
+  );
+};
+export default Error;
