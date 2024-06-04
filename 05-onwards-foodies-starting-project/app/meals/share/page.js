974888556b978a3c7b0d3f0378020539f81e1f6a
rizkirisjad <rizkirisjad@gmail.com>
2024-06-04 21:40:15 +0700

    image picker component

--- 05-onwards-foodies-starting-project/app/meals/share/page.js
+++ 05-onwards-foodies-starting-project/app/meals/share/page.js
@@ -1,5 +1,6 @@
 /** @format */
 
+import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 
 export default function ShareMealPage() {
@@ -40,7 +41,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          IMAGE PICKER
+          <ImagePicker />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
