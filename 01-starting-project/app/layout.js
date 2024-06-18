ecb00b6323a156e907eb48a1abf327d3ac6e64f2
rizkirisjad <rizkirisjad@gmail.com>
2024-06-18 14:38:50 +0700

    lanjutan kemarin

--- 01-starting-project/app/layout.js
+++ 01-starting-project/app/layout.js
@@ -1,14 +1,20 @@
+/** @format */
+
+import MainHeader from '@/components/MainHeader';
 import './globals.css';
 
 export const metadata = {
   title: 'Next.js Page Routing & Rendering',
   description: 'Learn how to route to different pages.',
-}
+};
 
 export default function RootLayout({ children }) {
- return (
+  return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <MainHeader />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
