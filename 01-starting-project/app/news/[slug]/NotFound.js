73e41fef649f8067d8ccf57bc2502d556673a713
rizkirisjad <rizkirisjad@gmail.com>
2024-06-19 20:48:51 +0700

    Handling "Not Found" Errors & Showing "Not Found" Pages

---
+++ 01-starting-project/app/news/[slug]/NotFound.js
@@ -0,0 +1,11 @@
+/** @format */
+
+const NotFoundPage = () => {
+  return (
+    <div id="error">
+      <h1>Not Found!</h1>
+      <p>\Unfortunately, we could not find the requested article.</p>
+    </div>
+  );
+};
+export default NotFoundPage;
