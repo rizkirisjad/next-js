f69a3eabebec244a73a002d81d1407a425dbf24a
rizkirisjad <rizkirisjad@gmail.com>
2024-07-16 20:58:27 +0700

    Providing User Feedback with The useFormStatus Hook

---
+++ mutating data/components/FormSubmit.js
@@ -0,0 +1,21 @@
+/** @format */
+'use client';
+
+import { useFormStatus } from 'react-dom';
+
+const FormSubmit = () => {
+  const status = useFormStatus();
+
+  if (status.pending) {
+    return <p>Creating post...</p>;
+  }
+
+  return (
+    <>
+      <button type="reset">Reset</button>
+      <button>Create Post</button>
+    </>
+  );
+};
+
+export default FormSubmit;
